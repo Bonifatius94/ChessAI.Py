f7e07640283b5091d2703971d9955ffe4fcec4df
Marco Tröster <marco@troester-gmbh.de>
2020-09-23 22:24:24 +0200

    added training function for custom reinforcement algorithm, no DQN anymore

--- src/reinf-chessai.py
+++ src/reinf-chessai.py
@@ -4,20 +4,61 @@ from tensorflow import keras
 import chesslib
 
 
+# training settings
+mutations_count = 1000
+training_epoch = 100
+
+
 def main():
 
     # start the training
-    do_training()
+    do_reinforcement_learning()
 
 
-def do_training():
+def do_reinforcement_learning():
+
+    # init keras model for first iteration
+    best_estimator = keras.Sequential([
+        keras.layers.Flatten(input_shape=(14,)),
+        keras.layers.Dense(128, activation='sigmoid'),
+        keras.layers.Dense(128, activation='sigmoid'),
+        keras.layers.Dense(128, activation='sigmoid'),
+        keras.layers.Dense(128, activation='sigmoid'),
+        keras.layers.Dense(128, activation='sigmoid'),
+        keras.layers.Dense(128, activation='sigmoid'),
+        keras.layers.Dense(128, activation='sigmoid'),
+        keras.layers.Dense(128, activation='sigmoid'),
+        keras.layers.Dense(128, activation='sigmoid'),
+        keras.layers.Dense(128, activation='sigmoid'),
+        keras.layers.Dense(1, activation='linear')
+    ])
+
+    # compile the network
+    best_estimator.compile(
+        optimizer='adam',
+        loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+        metrics=['accuracy']
+    )
+
+    # do endless training loop
+    while True:
+
+        # do reinforcement training
+        for i in range(training_epoch):
+
+            # 
+
+
+        # do evaluation vs. stockfish engine
+
 
     # TODO: implement training function
 
     # neuronal network:
     # =================
     # input: 14 x 64-bit integer array
-    # 7 layers with 128 neurons, sigmoid act. func.
+    # some layers with 128 neurons each, 
+    # maybe sigmoid activation function (still needs to be tested)
     # output: score 0.0 - 1.0
 
     # algorithm:
@@ -48,6 +89,65 @@ def do_training():
     # -> determine if the learned behavior is actually successful
 
     return 0
+
+
+class TrainingSession(object):
+
+    def __init__(self, best_estimator):
+
+        # init training variables
+        self.best_estimator = best_estimator
+        self.mutated_estimator = best_estimator
+        self.wins = 0
+        self.ties = 0
+        self.defeats = 0
+
+        # init chess game variables
+        self.board = chesslib.ChessBoard_StartFormation()
+        self.drawing_side = 0
+        self.draw_history = []
+
+
+    def do_training(self):
+
+        # TODO: implement this function
+        pass
+
+
+    def play_chessgame(self):
+
+        # reset it chess game variables
+        self.board = chesslib.ChessBoard_StartFormation()
+        self.drawing_side = 0
+        self.draw_history = []
+
+        # 
+
+
+    def is_game_over(self, step_draw):
+
+        # don't compute this function if the board is still in start formation
+        if len(self.draw_history) == 0:
+            return False
+
+        # determine whether the game is over
+        last_draw = self.draw_history[-] if len(self.draw_history) >= 1 else cheslib.ChessDraw_Null
+        state = chesslib.GameState(self.board, last_draw)
+        enemy_draws = chesslib.GenerateDraws(self.board, self.drawing_side, step_draw, True)
+
+        # update the game's outcome stats if the game is over (win / loss / tie)
+        if state != chesslib.GameSate_Checkmate:
+            self.defeats += 1
+        elif state != chesslib.GameState_Tie:
+            self.ties += 1
+        elif len(enemy_draws) == 0:
+            self.wins += 1
+        # otherwise just continue (game is not over yet)
+        else:
+            return False
+
+        # some game-over condition (win / defeat / tie) applied before
+        return True
 
 
 # start main function
