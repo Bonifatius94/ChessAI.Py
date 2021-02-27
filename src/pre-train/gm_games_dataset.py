689ec315e241974cc0df89a76440608e73a0be2d
Marco Tröster <marco@troester-gmbh.de>
2021-02-27 13:57:58 +0100

    made bitboard to 2D feature maps conversion work (tested)

--- src/pre-train/gm_games_dataset.py
+++ src/pre-train/gm_games_dataset.py
@@ -57,7 +57,7 @@ def prepare_pandas_data(pd_winrates: pd.DataFrame):
 
     # convert the dataframe into SARS data slices (state-action-reward-nextstate)
     states = [bitboards_from_hash(x) for x in pd_winrates['BoardBeforeHash']]
-    actions = [compact_draw_from_hash(x) for x in pd_winrates['DrawHashNumeric']]
+    actions = pd_winrates['DrawHashNumeric']
     #actions = [int(x) for x in pd_winrates['DrawHashNumeric']]
     #compact_actions = [compact_draw_from_hash(x) for x in pd_winrates['DrawHashNumeric']]
     rewards = pd_winrates['WinRate']
@@ -87,15 +87,21 @@ def compact_draw_from_hash(draw_hash: str):
 
 def create_tf_dataset(pd_sars_winrates: pd.DataFrame, batch_size: int, train: bool):
 
+    print(pd_sars_winrates.dtypes)
+
     # convert the pandas dataframe into a tensor dataset
-    dataset = tf.data.Dataset.from_tensor_slices(pd_sars_winrates)
+    print(type(pd_sars_winrates['states']))
+    states =  pd_sars_winrates['states'].apply(np.array)
+    np_states = states.apply(np.array)
+    print(states.head(), np_states)
+    dataset = tf.data.Dataset.from_tensor_slices(pd_sars_winrates.values)
 
     # map the chesslib bitboards to convolutable 8x8x7 feature maps
     dataset = dataset.map(lambda x: (
-        chessboard_to_feature_maps(x[0]),    # state
-        x[1],                                # action
-        x[2],                                # reward
-        chessboard_to_feature_maps(x[3])))   # next state
+        chessboard_to_2d_feature_maps(x[0]),    # state
+        x[1],                                   # action
+        x[2],                                   # reward
+        chessboard_to_2d_feature_maps(x[3])))   # next state
 
     # batch the data properly
     dataset = dataset.batch(batch_size)
@@ -107,7 +113,27 @@ def create_tf_dataset(pd_sars_winrates: pd.DataFrame, batch_size: int, train: bo
 
 
 @tf.function
-def chessboard_to_feature_maps(bitboards):
+def chessboard_to_2d_feature_maps(bitboards):
+
+    # extract each bit from the bitboards -> bring it into 13x8x8 shape
+    feature_maps = _chessboard_to_feature_maps(bitboards)
+
+    # union the pieces of each type onto a single feature map and encode it
+    # as follows: white piece = 1, black piece = -1, nothing = 0
+    # the resulting maps are of shape 6x8x8
+    unified_piece_maps = feature_maps[0:6, :, :] + (feature_maps[6:12, :, :] * -1.0)
+
+    # append the was_moved bitboard to piece maps -> 6x8x8 shape
+    compressed_feature_maps = tf.concat((unified_piece_maps, feature_maps[12:13, :, :]), axis=0)
+
+    # transpose the feature maps, so the content can be convoluted
+    # by field positions and maps are the channels -> shape 8x8x7
+    transp_feature_maps = tf.transpose(compressed_feature_maps, (1, 2, 0))
+    return transp_feature_maps
+
+
+@tf.function
+def _chessboard_to_feature_maps(bitboards):
 
     # create empty tensor array for bitboards
     feature_maps = tf.TensorArray(dtype=tf.float32, size=13, dynamic_size=False)
@@ -122,42 +148,30 @@ def chessboard_to_feature_maps(bitboards):
         # loop through all bitboard positions
         for pos in tf.range(64):
             # extract the piece_set bit from the bitboard: (bitboard >> pos) & 1
-            bit = tf.bitwise.bitwise_and(tf.bitwise.right_shift(temp_bitboard, pos), 1)
-            temp_feature_map.write(pos, tf.cast(bit, tf.float32))
+            bit = tf.bitwise.bitwise_and(tf.bitwise.right_shift(temp_bitboard, tf.cast(pos, dtype=tf.uint64)), 1)
+            temp_feature_map = temp_feature_map.write(pos, tf.cast(bit, tf.float32))
 
         # reshape the 64 bitboard positions to 8x8 format, dimensions are (row, column)
+        temp_feature_map = temp_feature_map.stack()
         temp_feature_map = tf.reshape(temp_feature_map, (8, 8))
 
         # apply the converted feature map to the output
-        feature_maps.write(i, temp_feature_map)
+        feature_maps = feature_maps.write(i, temp_feature_map)
 
+    feature_maps = feature_maps.stack()
     return feature_maps
-
-
-@tf.function
-def chessboard_to_2d_feature_maps(bitboards):
-
-    # extract each bit from the bitboards -> bring it into 13x8x8 shape
-    feature_maps = chessboard_to_feature_maps(bitboards)
-
-    # union the pieces of each type onto a single feature map and encode it
-    # as follows: white piece = 1, black piece = -1, nothing = 0
-    # the resulting maps are of shape 6x8x8
-    unified_piece_maps = feature_maps[0:6, :, :] + (feature_maps[6:12, :, :] * -1.0)
-
-    # append the was_moved bitboard to piece maps -> 6x8x8 shape
-    compressed_feature_maps = tf.concat((unified_piece_maps, feature_maps[12:13, :, :]), axis=0)
-
-    # transpose the compressed feature maps, so the content can be convoluted
-    # by field positions and maps are the channels -> final shape 8x8x7
-    return tf.transpose(compressed_feature_maps, (1, 2, 0))
 
 
 if __name__ == '__main__':
 
-    # test loading training and eval datasets
-    batch_size = 32
-    train_dataset, eval_dataset = load_datasets(batch_size)
+    # test single chessboard tensor conversion
+    start_board = chesslib.ChessBoard_StartFormation()
+    compressed_start_board = chessboard_to_2d_feature_maps(start_board)
+    print(compressed_start_board.numpy())
 
-    print(train_dataset)
-    print(eval_dataset)
+    # # test loading training and eval datasets
+    # batch_size = 32
+    # train_dataset, eval_dataset = load_datasets(batch_size)
+
+    # print(train_dataset)
+    # print(eval_dataset)
